f493ee5e41ba2ca2bc762afff43b1b45f354997a
Vladimir Kozlov <vkozloff@gmail.com>
2021-12-30 14:19:37 +0200

    Refactored

--- mqtt2txt.py
+++ mqtt2txt.py
@@ -8,12 +8,14 @@ import paho.mqtt.client as mqttClient
 import time
 import os
 import logging
+import signal
 from pathlib import Path
 from datetime import datetime
 
 
 files = dict()
 files_path = '/home/vkozlov/'
+closing = False
 
 _LOGGER = logging.getLogger(__name__)
 _LOGGER.setLevel(logging.DEBUG)
@@ -42,7 +44,8 @@ class RollingFileProcessor:
         return False
 
     def render_data(self, str_data):
-        self._file_handle.write(str_data + '\n')
+        ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S ')
+        self._file_handle.write(ts + str_data + '\n')
 
     def execute(self, str_data):
         self.prepare_file()
@@ -75,23 +78,41 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, message):
-    print("Message received: " + message.topic + " => " + str(message.payload, 'utf-8'))
-    processor = get_processor_for_topic(message.topic, files_path)
-    processor.execute(str(message.payload, 'utf-8'))
+    # print("Message received: " + message.topic + " => " + str(message.payload, 'utf-8'))
+    if not closing:
+        processor = get_processor_for_topic(message.topic, files_path)
+        processor.execute(str(message.payload, 'utf-8'))
+
+
+def close_all():
+    closing = True
+    for value in files.values():
+        print('closing')
+        value.flush()
+
+
+def handler_stop_signals(signum, frame):
+    close_all()
 
 Connected = False   #global variable for the state of the connection
 
 
-
 broker_address= "192.168.0.114"  #Broker address
-broker_port = 1883                         #Broker port
+broker_port = 1883               #Broker port
 
 client = mqttClient.Client(clean_session=False, client_id='mqtt2txt')    #create new instance
 client.on_connect= on_connect                      #attach function to callback
 client.on_message= on_message                      #attach function to callback
+# client.on_disconnect= on_disconnect
 client.connect(broker_address, broker_port, 60)    #connect
-client.subscribe("some/topic") #subscribe
-client.subscribe("some/topic2")
-client.loop_forever() #then keep listening forever
+client.subscribe("I2S/rawRMS") #subscribe
 
-# TODO close all files
+
+signal.signal(signal.SIGTERM, handler_stop_signals)
+
+try:
+    client.loop_forever() #then keep listening forever
+except KeyboardInterrupt:
+    pass
+finally:
+    close_all()
